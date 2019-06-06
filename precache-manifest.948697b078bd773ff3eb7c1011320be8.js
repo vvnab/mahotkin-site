d228c11c1819633f26a73889f1797549b764bf2e
Виталий Набережный <naberezhniy@taxi21.ru>
2019-06-06 09:51:51 +0300

    Updates

---
+++ precache-manifest.948697b078bd773ff3eb7c1011320be8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ca093fb6b5a756fd8995b05fdbaed53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b78117d6ffcaf8732db5",
+    "url": "/static/css/main.5a69a706.chunk.css"
+  },
+  {
+    "revision": "ff37603e666735ea51e4",
+    "url": "/static/js/2.5a0ac9fd.chunk.js"
+  },
+  {
+    "revision": "b78117d6ffcaf8732db5",
+    "url": "/static/js/main.8c9329e1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f1752cdc92408d3d952bdf888192d735",
+    "url": "/static/media/img_bg1.f1752cdc.jpg"
+  }
+]);
